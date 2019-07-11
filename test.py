f46fd8a69cfa9ae50e9e0bf9e05da2f9d8ec9484
wangxin1009 <wangxinpm1009@gmail.com>
2019-07-11 12:39:35 +0800

    auto bot

--- test.py
+++ test.py
@@ -1,13 +1,30 @@
-class Employee:
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-    def __init__(self, first, last, pay):
-        self.first = first 
-        self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@company.com'
+usernameStr = 'putYourUsernameHere'
+passwordStr = 'putYourPasswordHere'
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+browser = webdriver.Chrome()
+browser.get(('https://accounts.google.com/ServiceLogin?'
+             'service=mail&continue=https://mail.google'
+             '.com/mail/#identifier'))
 
-print(emp_1.email)
-print(emp_2.email)
+# fill in username and hit the next button
+
+username = browser.find_element_by_id('identifierId')
+username.send_keys(usernameStr)
+
+nextButton = browser.find_element_by_id('identifierNext')
+nextButton.click()
+
+# wait for transition then continue to fill items
+
+password = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.NAME, "password")))
+
+password.send_keys(passwordStr)
+
+signInButton = browser.find_element_by_id('passwordNext')
+signInButton.click()
